54bc5db206c8cb97ebbf6064f7986ab581bf5320
Ilyes Kadji <web.kin.dev@gmail.com>
2023-09-15 11:39:52 +0200

    create server

---
+++ api/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const app = require('./app');
+
+// Normalize the port
+const normalizePort = val => {
+      const port = parseInt(val, 10);
+
+      if (isNaN(port)) {
+            return val;
+      }
+      if (port >= 0) {
+            return port;
+      }
+      return false;
+};
+
+// Set the port
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Error handler for server
+const errorHandler = error => {
+      if (error.syscall !== 'listen') {
+            throw error;
+      }
+      const address = server.address();
+      const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+      switch (error.code) {
+      case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+      case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+      default:
+            throw error;
+      }
+};
+
+
+// Create server
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+      const address = server.address();
+      const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+      console.log('Listening on ' + bind);
+});
+
+// Start server
+server.listen(port);
